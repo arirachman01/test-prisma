321c205c08024a964e22e0c805a084705b7d909b
ZeroNoSho <inukuro351@gmail.com>
2026-09-23 08:23:15 +0700

    update

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -5,7 +5,13 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
+    const adapter = new PrismaMariaDb({
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+    });
 
     super({ adapter });
   }
